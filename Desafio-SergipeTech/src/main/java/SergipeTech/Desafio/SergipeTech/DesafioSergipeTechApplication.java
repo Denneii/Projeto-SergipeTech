2116c4feecc1a472f7cfba6d2af834ca09af8707
Dennis <106123685+Denneii@users.noreply.github.com>
2025-01-28 14:05:28 -0300

    Estrutura do spring

---
+++ Desafio-SergipeTech/src/main/java/SergipeTech/Desafio/SergipeTech/DesafioSergipeTechApplication.java
@@ -0,0 +1,13 @@
+package SergipeTech.Desafio.SergipeTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSergipeTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSergipeTechApplication.class, args);
+	}
+
+}
---
+++ Desafio-SergipeTech/src/test/java/SergipeTech/Desafio/SergipeTech/DesafioSergipeTechApplicationTests.java
@@ -0,0 +1,13 @@
+package SergipeTech.Desafio.SergipeTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSergipeTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
